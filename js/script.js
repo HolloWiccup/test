6ff4c1a747e5725e1194073075b2dbdb68606df4
Dimitry Nasrtdinov <75541143+HolloWiccup@users.noreply.github.com>
2022-11-07 13:44:00 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,15 +37,15 @@ function calcHandler(e){
             printNum(0);
         }
         if(button.id === 'C'){
-            printNum(calculator.b.length ? 
-                calculator.b = calculator.b.slice(0, -1) : calculator.a.length ? 
-                    calculator.a = calculator.a.slice(0, -1) : 0);
+            if(board.outerHTML.includes(calculator.b) && calculator.b !== ''){
+                printNum((calculator.b = calculator.b.slice(0, -1)) || calculator.a);
+            }else if(board.outerHTML.includes(calculator.a)){
+                printNum((calculator.a = calculator.a.slice(0, -1)) || '0');       
+            }else{
+                printNum(calculator.b || calculator.a);
+            }
         }
     }
-
-
-
-
     console.log(button);
 }
 
@@ -55,19 +55,34 @@ const numIsEmpty = () =>
 const printNum = (num) =>
     board.innerHTML = num;
 
+function fixResult(str){
+    let result = '';
+    let index = str.indexOf('.');
+    if(str.length > 10 && (index < 0 || index > 9)){
+    result = `${str.slice(0, 8)}e${str.length -8}`;
+    }else if(index > 0 && index < 10){
+        result = str.slice(0, 10);
+    }else{
+        result = str;
+    }
+    return result;
+}
+
 function Calculator(){
     this.a = '';
     this.op = '';
     this.b = '';
 
     this.methods = {
-        '+': (a, b) => +a + +b,
-        '-': (a, b) => a - b,
-        '*': (a, b) => a * b,
-        '/': (a, b) => a / b,
+        '+': (a, b) => (a * 10 + b * 10) / 10,
+        '-': (a, b) => (a * 10 - b * 10) / 10,
+        '*': (a, b) => +a * +b,
+        '/': (a, b) => (+a / +b),
+        '%': (a, b) => +a / 100 * +b,
     }
     this.calculate = () => 
-        this.methods[this.op](this.a, this.b) + '';
+        // this.methods[this.op](this.a, this.b) + '';
+        fixResult(this.methods[this.op](this.a, this.b) + '');
 
 }
 
